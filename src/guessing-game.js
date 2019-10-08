2c2b6b8228bfdfebddd0125e136e97a1d910be5b
maryna-maksimenka <maksimenka.maryna@gmail.com>
2019-10-08 23:15:10 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+    return this.guessNumber = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
